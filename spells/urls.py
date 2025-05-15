bb6f9ebf547bc2093c826f865121c97f111c2b79
Kyle-D-616 <kyled616@gmail.com>
2025-05-15 00:36:23 -0400

    seting up inital spells list url/view

--- spells/urls.py
+++ spells/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.spellList, name="spell_list"),
+    path("", views.spellList, name="spellList"),
 ]
--- spells/views.py
+++ spells/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from .models import Spell
 
+
 def spellList(request):
+    selectedList = request.GET.get('spellList')
 
+    if selectedList:
+        spells = Spell.objects.filter(spellList__incontains=selectedList)
+    else:
+        spells = spells.objects.all()
 
-    spells = Spell.object.all().order_by('name')
-    return render(request, 'spells/spell_list.html', {'spells': spells})
+    spellListOptions = Spell.objects.values_list('spellList', flat=True).distinct()
+
+    return render(request, 'spells/spellList.html', {
+        'spells': spells,
+        'spellListOptions': spellListOptions,
+        'selectedList': selectedList
+    })
