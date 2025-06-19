e169e2b625a92cd05509307ec09522226bdb1c07
Kyle-D-616 <kyled616@gmail.com>
2025-06-19 00:34:13 -0400

    set up front end to look ok. in the middle of implementing sessions to store user data without log in. need to clean up view then make templates

--- spells/views.py
+++ spells/views.py
@@ -3,21 +3,29 @@ from .models import Spell2014, SpellList2014, Spell2024, SpellList2024
 
 
 def spellList(request):
-    version = request.GET.get('version', '2024')
+    version = request.GET.get('version','2024')
     selectedSpellList = request.GET.getlist('spellList')
 
-    if version == '2024':
-        spells = Spell2024.objects.all()
-        if selectedSpellList:
-            spells = Spell2024.filter(spellList__name__in=selectedSpellList)
-        spellListOptions = SpellList2024.objects.all()
-    else:
-        spells = Spell2014.objects.all()
-        if selectedSpellList:
-            spells = Spell2014.filter(spellList__name__in=selectedSpellList)
-        spellListOptions = SpellList2014.objects.all()
+    if request.method == 'POST':
+        selectedSpellIds = request.POST.getlist('selected_spells')
+        request.session['selectedSpells'] = [int(id) for id in selectedSpellList]
+        request.session.modified = True
 
-    return render(request, 'spells/spellList.html', {
+        selectedSpells = request.session.get('selectedSpells', [])
+
+        if version == '2024':
+            spells = Spell2024.objects.all()
+            if selectedSpellList:
+                spells = spells.filter(spellList__name__in=selectedSpellList)
+                spellListOptions = SpellList2024.objects.all()
+            else:
+                spells = Spell2014.objects.all()
+                if selectedSpellList:
+                    spells = spells.filter(spellList__name__in=selectedSpellList)
+                    spellListOptions = SpellList2014.objects.all()
+
+
+        return render(request, 'spells/spellList.html', {
         'version': version,
         'spells': spells,
         'spellListOptions': spellListOptions,
