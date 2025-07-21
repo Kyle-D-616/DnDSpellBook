2961138421d518169929f2914ba158bc460fc86f
Kyle <kyled616@gmail.com>
2025-07-20 22:36:38 -0400

    got test script up and running, spell keys are returning, need to clean the data the are returning.

--- spells/services/spellScraper/spellScraper.py
+++ spells/services/spellScraper/spellScraper.py
@@ -15,22 +15,29 @@ class SpellScraper:
             baseUrl = 'http://dnd2024.wikidot.com/spell:all'
         response = self.session.get(baseUrl)
         soup = BeautifulSoup(response.content, 'html.parser')
-        table = soup.find('table')
-        for url in table.find_all('a'):
-            self.spellUrls.append('https://dnd5e.wikidot.com' + url.get('href'))
+        tables = soup.find_all('table')
+        for table in tables:
+            for url in table.find_all('a'):
+                if self.version == 2014:
+                    self.spellUrls.append('https://dnd5e.wikidot.com' + url.get('href'))
+                else:
+                    self.spellUrls.append('http://dnd2024.wikidot.com' + url.get('href'))
+ 
 
     def getSpellData(self, spellUrls):
         for spellUrl in self.spellUrls:
             response = self.session.get(spellUrl)
             soup = BeautifulSoup(response.content, 'html.parser')
-            #make name then make spell key to do check
             # Spell name
             spellDiv = soup.find('div', {'class': 'page-title page-header'})
             spellName = spellDiv.find('span').get_text(strip=True)
             spellKey = str(self.version) + spellName.replace(" ", "")
             print(spellKey)
+            #spell keys are returning, need to take symbols out of key tho, ' ( ) and / are the ones i noticed, go see what the (UA) is, maybe i want to keep it?
+
         # Check if the spell already exists in the database
 #        if Spells.objects.filter(name=spellName).exists():
 #            self.stdout.write(self.style.SUCCESS(f"Spell '{spellName}' already exists, skipping..."))
 #            continue  # Skip to the next spell if it already exists
 #
+
---
+++ test_spell_scraper.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+import django
+
+# Setup Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DnDSpellBook.settings')
+django.setup()
+
+from spells.services.spellScraper.spellScraper import SpellScraper
+
+def test_spell_scraper():
+    print("Testing SpellScraper...")
+    
+    # Create scraper instance
+    version = int(input("Which version?"))
+    if version == 2014:
+        baseUrl = 'https://dnd5e.wikidot.com/spells:'
+    else:
+        baseUrl = 'http://dnd2024.wikidot.com/spell:'
+    scraper = SpellScraper(version)
+
+    
+    # Get all spell URLs
+    print("Getting all spell URLs...")
+    scraper.getSpellUrls('https://dnd5e.wikidot.com/spells')
+    #print(f"Found {len(scraper.spellUrls)} spell URLs")
+    
+    # Call getSpellData to process all URLs
+    print("Processing all spells...")
+    scraper.getSpellData(None)
+    print("Test completed!")
+
+if __name__ == "__main__":
+    test_spell_scraper()
