a910d713af155e2fccb4e54b42ee76bc506ba8df
Kyle-D-616 <kyled616@gmail.com>
2025-08-20 23:01:08 -0400

    finished with spell lists saver, need to test saving a spell to db. need command to call new classes to scrape and save a spell

--- spells/services/spellSaver.py
+++ spells/services/spellSaver.py
@@ -20,15 +20,14 @@ class SpellSaver:
 
         spell.save()
 
-    def save_spelllist_to_database(self):
         list_objs = []
-        for list_name in self.spell_lists:
-            self.list_name = self.list_name.strip()
-            if not self.list_name:
+        for list_name in self.spellData.get('spellLists',[]):
+            list_name = list_name.strip()
+            if not list_name:
                 continue
             spell_list_obj, _ = SpellList.objects.get_or_create(name=list_name)
             list_objs.append(spell_list_obj)
 
         spell.spelllist.set(list_objs)
 
- 
+        return spell
